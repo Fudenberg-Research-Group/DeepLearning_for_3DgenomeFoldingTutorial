61319780ac55ac7d6b39df05e3cf4a48b0ae6efa
Yaoyx <yxiaoy@umich.edu>
2024-06-24 17:27:56 -0700

    change file structure

--- manuscripts/cross2020/get_models.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wget https://storage.googleapis.com/basenji_barnyard2/model_human.h5
-wget https://storage.googleapis.com/basenji_barnyard2/model_mouse.h5
-
-# also have .tf but they are directories, so easier to use gsutil cp -r
--- manuscripts/genome_research2018/get_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://storage.googleapis.com/131k/sample_wigs.txt
-wget https://storage.googleapis.com/131k/l131k_w128.bed
-wget https://storage.googleapis.com/131k/l131k_w128.h5
